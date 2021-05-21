29faecdb6de4437672c6a573a870961472aca864
Halford1993 <82106017+Halford1993@users.noreply.github.com>
2021-05-21 12:35:54 +0200

    Incluye clase pojo

---
+++ app/src/main/java/POJO/Perfil.java
@@ -0,0 +1,91 @@
+package POJO;
+
+public class Perfil {
+    private int id;
+    private String nombre, poblacion, fecha_nac, perfil_spotify, usuario, mail;
+
+    public Perfil(){
+
+    }
+
+    //Recordar que el id se genera automáticamente con el registro en la db, así que no debemos definirlo nosotros
+    //si fuésemos a hacer un registro nuevo, deberíamos dejarlo en null
+    public Perfil(int id, String nombre, String poblacion, String fecha_nac, String perfil_spotify, String usuario, String mail) {
+        this.id = id;
+        this.nombre = nombre;
+        this.poblacion = poblacion;
+        this.fecha_nac = fecha_nac;
+        this.perfil_spotify = perfil_spotify;
+        this.usuario = usuario;
+        this.mail = mail;
+    }
+
+    public void setId(){
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPoblacion() {
+        return poblacion;
+    }
+
+    public void setPoblacion(String poblacion) {
+        this.poblacion = poblacion;
+    }
+
+    public String getFecha_nac() {
+        return fecha_nac;
+    }
+
+    public void setFecha_nac(String fecha_nac) {
+        this.fecha_nac = fecha_nac;
+    }
+
+    public String getPerfil_spotify() {
+        return perfil_spotify;
+    }
+
+    public void setPerfil_spotify(String perfil_spotify) {
+        this.perfil_spotify = perfil_spotify;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    @Override
+    public String toString() {
+        return "Perfil{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", poblacion='" + poblacion + '\'' +
+                ", fecha_nac='" + fecha_nac + '\'' +
+                ", perfil_spotify='" + perfil_spotify + '\'' +
+                ", usuario='" + usuario + '\'' +
+                ", mail='" + mail + '\'' +
+                '}';
+    }
+}
--- app/src/main/java/com/example/vinyl/MainActivity.java
+++ app/src/main/java/com/example/vinyl/MainActivity.java
@@ -5,13 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etUsuario, etPass;
+    Button btLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etUsuario = findViewById(R.id.et_user);
+        etPass = findViewById(R.id.et_pass);
+        btLogin = findViewById(R.id.bt_login);
+
+        Bundle extras = getIntent().getExtras();
+        if(extras!=null){
+            etUsuario.setText(extras.getString("usuarioIntent"));
+        }
     }
 
     public void registro(View view){
